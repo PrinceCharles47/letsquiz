6ac2b6276e2207fbb4d17b258d8b2086a0c776e6
PrinceCharles47 <clemente.pcharles@gmail.com>
2023-04-12 21:48:21 +0700

    add initial create quiz ui

---
+++ src/components/ChoiceInput.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="d-flex align-center mx-4 mb-2 pa-1 choice-container">
+        <v-radio />
+        <input type="text" placeholder="Type here..." style="width: 100%;">
+        <v-icon color="red">mdi-close</v-icon>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'ChoiceInput',
+    data: () => ({
+
+    })
+}
+</script>
+<style scoped>
+.choice-container {
+    border: 1px solid #d9d9d9;
+    border-radius: 5px;
+}
+</style>
---
+++ src/views/CreateQuiz.vue
@@ -0,0 +1,28 @@
+<template>
+    <v-container>
+        <v-row>
+            <v-col cols="12" sm="8" md="6" class="mx-auto">
+                <!-- <v-btn block>Save Quiz</v-btn> -->
+                <new-quiz/>
+            </v-col>
+        </v-row>
+    </v-container>
+</template>
+
+<script>
+import NewQuiz from '@/components/NewQuiz.vue';
+
+export default {
+    name: 'CreateQuiz',
+    components: {
+        NewQuiz
+    },
+    data: () => ({
+
+    })
+}
+</script>
+
+<style>
+    
+</style>
