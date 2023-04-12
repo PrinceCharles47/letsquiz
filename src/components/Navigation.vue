0ad3771e9e4e8ab9b97f8611d5eb6c94c8a48379
PrinceCharles47 <clemente.pcharles@gmail.com>
2023-04-12 21:47:33 +0700

    add initial nav bar

---
+++ src/components/Navigation.vue
@@ -0,0 +1,27 @@
+<template>
+    <v-app-bar app dense flat id="navbar" color="white">
+        <p class="mt-4">Let's <span class="font-weight-bold purple--text">QUIZ</span></p>
+        <v-btn outlined class="ml-auto">Log Out</v-btn>
+    </v-app-bar>
+</template>
+  
+<script>
+
+export default {
+    name: 'Navigation',
+
+    data: () => ({
+
+    }),
+    methods: {
+
+    }
+};
+</script>
+  
+<style>
+/* #navbar {
+    background: transparent;
+} */
+</style>
+  
