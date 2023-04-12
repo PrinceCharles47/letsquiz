4dd2dc2a256c97ffdccc1a5cdd91c63bab9caf33
PrinceCharles47 <clemente.pcharles@gmail.com>
2023-04-12 21:47:53 +0700

    add initial dashboard ui

---
+++ src/components/QuizCard.vue
@@ -0,0 +1,21 @@
+<template>
+    <v-card flat outlined class="mb-2">
+        <v-card-title>Fundamentals of Programming (Java Programming)</v-card-title>
+        <v-card-text class="d-flex">
+            <p class="mr-6">40 Items</p>
+            <p>April 12, 2023</p>
+        </v-card-text>
+    </v-card>
+</template>
+
+<script>
+export default {
+    name: 'QuizCard',
+    data: () => ({
+        test: 'testing'
+    })
+}
+</script>
+<style lang="">
+    
+</style>
---
+++ src/views/Dashboard.vue
@@ -0,0 +1,39 @@
+<template>
+    <v-container>
+        <v-row>
+            <v-col cols="12" sm="8" md="6" class="mx-auto">
+                <v-btn :block="block">Create Quiz</v-btn>
+                <v-card-title class="pl-0">My Quizzes</v-card-title>
+                <quiz-card />
+                <quiz-card />
+                <quiz-card />
+                <quiz-card />
+                <quiz-card />
+                <quiz-card />
+            </v-col>
+        </v-row>
+    </v-container>
+</template>
+
+<script>
+import QuizCard from '@/components/QuizCard.vue';
+
+export default {
+    name: 'Dashboard',
+
+    data: () => ({
+        block: true     
+    }),
+    components: {
+        QuizCard
+    }
+}
+</script>
+
+<style scoped>
+
+/* *{
+    border: 1px solid grey;
+} */
+
+</style>
